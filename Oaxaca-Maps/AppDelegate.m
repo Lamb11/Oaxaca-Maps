87f3e4b1e0c2dc5afa7846811e21b3f5df89bb88
Lamb11 <abc_1118@hotmail.com>
2015-02-15 17:37:49 -0600

    Añadir frameworks que se necesitan. Añadir el APIKEY en AppDelegate.

--- Oaxaca-Maps/AppDelegate.m
+++ Oaxaca-Maps/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <GoogleMaps/GoogleMaps.h>
 
 @interface AppDelegate ()
 
@@ -17,6 +18,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    [GMSServices provideAPIKey:@"AIzaSyDTaYm8AcrM7uVtr0jvSP7Ptxarvfkwv9M"];
     return YES;
 }
 
